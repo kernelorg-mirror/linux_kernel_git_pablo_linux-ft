62b5feccef6ac0a762825077f11b784eac4de5bf
Pablo Neira Ayuso <pablo@netfilter.org>
2023-01-27 11:50:06 +0100

    xfrm: add fill forward path for xfrm interface
    include "flowtable: fix xfrm datapath establishment"
    
    don't use skb_dst(skb) in dev_fill_forward_path(), it does not work to
    infer reverse path.
    
    do not use xfrm_decode_session(), it refers to the original path only.
    
    adjust input ifindex to the tunnel device for decapsulated tunnel packets.
    
    cache original route (only required for route object, not for flow_tuple
    actually, dst_release on the xfrm bundle is sufficient).
    
    do not hold refcnt on dst: ctx.dst already holds reference count and
    xfrm_lookup_with_ifid() already provides a reference counter of 1.

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -897,6 +897,7 @@ enum net_device_path_type {
 	DEV_PATH_BRIDGE,
 	DEV_PATH_PPPOE,
 	DEV_PATH_DSA,
+	DEV_PATH_TUNNEL,
 	DEV_PATH_MTK_WDMA,
 };
 
@@ -924,6 +925,9 @@ struct net_device_path {
 			u16 proto;
 		} dsa;
 		struct {
+			struct dst_entry        *dst;
+		} tun;
+		struct {
 			u8 wdma_idx;
 			u8 queue;
 			u16 wcid;
@@ -943,6 +947,9 @@ struct net_device_path_stack {
 struct net_device_path_ctx {
 	const struct net_device *dev;
 	u8			daddr[ETH_ALEN];
+
+	struct flowi		flowi;
+	struct dst_entry	*dst;
 
 	int			num_vlans;
 	struct {
@@ -3028,6 +3035,7 @@ void dev_remove_offload(struct packet_offload *po);
 int dev_get_iflink(const struct net_device *dev);
 int dev_fill_metadata_dst(struct net_device *dev, struct sk_buff *skb);
 int dev_fill_forward_path(const struct net_device *dev, const u8 *daddr,
+			  struct dst_entry *dst, struct flowi *flowi,
 			  struct net_device_path_stack *stack);
 struct net_device *__dev_get_by_flags(struct net *net, unsigned short flags,
 				      unsigned short mask);
