a2b4b6022d59e911723a58da501f2dcee1519dd3
Pablo Neira Ayuso <pablo@netfilter.org>
2023-04-14 11:55:37 +0200

    xfrm: possible out-of-bounds memory access in decode_session6()
    decode_session6() assumes IP6CB() area is in place, but this is not the
    case with the flowtable.
    
    Apr 12 23:10:38 peppermint kernel: [  110.594615] ==================================================================
    Apr 12 23:10:38 peppermint kernel: [  110.594700] BUG: KASAN: slab-out-of-bounds in decode_session6+0x79/0x7b0
    Apr 12 23:10:38 peppermint kernel: [  110.594760] Read of size 1 at addr ffff8881144658f7 by task iperf3/750
    Apr 12 23:10:38 peppermint kernel: [  110.594812]
    Apr 12 23:10:38 peppermint kernel: [  110.594830] CPU: 4 PID: 750 Comm: iperf3 Not tainted 6.2.0-rc8+ #139
    Apr 12 23:10:38 peppermint kernel: [  110.594882] Hardware name: LENOVO 2349N7G/2349N7G, BIOS G1ET98WW (2.58 ) 10/23/2013
    Apr 12 23:10:38 peppermint kernel: [  110.594940] Call Trace:
    Apr 12 23:10:38 peppermint kernel: [  110.594964]  <IRQ>
    Apr 12 23:10:38 peppermint kernel: [  110.594985]  dump_stack_lvl+0x34/0x44
    Apr 12 23:10:38 peppermint kernel: [  110.595022]  print_address_description.constprop.0+0x87/0x1e4
    Apr 12 23:10:38 peppermint kernel: [  110.595072]  print_report+0x34/0x4d
    Apr 12 23:10:38 peppermint kernel: [  110.595106]  ? decode_session6+0x79/0x7b0
    Apr 12 23:10:38 peppermint kernel: [  110.595143]  kasan_report+0xae/0xe0
    Apr 12 23:10:38 peppermint kernel: [  110.595179]  ? decode_session6+0x79/0x7b0
    Apr 12 23:10:38 peppermint kernel: [  110.595216]  decode_session6+0x79/0x7b0
    Apr 12 23:10:39 peppermint kernel: [  110.595252]  ? flow_hash_from_keys+0xe3/0x1d0
    Apr 12 23:10:39 peppermint kernel: [  110.595294]  ? __skb_flow_bpf_to_target+0x330/0x330
    Apr 12 23:10:39 peppermint kernel: [  110.595339]  __xfrm_decode_session+0x40/0x50
    Apr 12 23:10:39 peppermint kernel: [  110.595377]  icmpv6_route_lookup+0x1aa/0x290 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.595506]  ? icmpv6_xrlim_allow+0x210/0x210 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.595629]  ? icmpv6_mask_allow+0x21/0x30 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.595750]  ? icmpv6_xrlim_allow+0x78/0x210 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.595873]  ? __xfrm_policy_check2.constprop.0+0x1a0/0x1a0 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.596004]  ? ip6_route_output_flags_noref+0xc0/0x150 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.596127]  ? selinux_xfrm_skb_sid_ingress+0xe0/0x120
    Apr 12 23:10:39 peppermint kernel: [  110.596174]  icmp6_send+0x8c8/0xe50 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.596294]  ? icmpv6_push_pending_frames+0x1b0/0x1b0 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.596422]  ? __rcu_read_unlock+0x2a/0x60
    Apr 12 23:10:39 peppermint kernel: [  110.596461]  ? avc_has_perm+0xe1/0x210
    Apr 12 23:10:39 peppermint kernel: [  110.596499]  ? rt6_probe+0x1d/0x480 [ipv6]
    Apr 12 23:10:39 peppermint kernel: [  110.596612]  ? nf_flow_offload_ip_hook_list+0x10/0x10 [nf_flow_table_inet]

--- net/xfrm/xfrm_policy.c
+++ net/xfrm/xfrm_policy.c
@@ -3434,20 +3434,14 @@ decode_session4(struct sk_buff *skb, struct flowi *fl, bool reverse)
 static void
 decode_session6(struct sk_buff *skb, struct flowi *fl, bool reverse)
 {
+	u8 nexthdr = ipv6_hdr(skb)->nexthdr;
 	struct flowi6 *fl6 = &fl->u.ip6;
 	int onlyproto = 0;
 	const struct ipv6hdr *hdr = ipv6_hdr(skb);
 	u32 offset = sizeof(*hdr);
 	struct ipv6_opt_hdr *exthdr;
 	const unsigned char *nh = skb_network_header(skb);
-	u16 nhoff = IP6CB(skb)->nhoff;
 	int oif = 0;
-	u8 nexthdr;
-
-	if (!nhoff)
-		nhoff = offsetof(struct ipv6hdr, nexthdr);
-
-	nexthdr = nh[nhoff];
 
 	if (skb_dst(skb) && skb_dst(skb)->dev)
 		oif = skb_dst(skb)->dev->ifindex;
