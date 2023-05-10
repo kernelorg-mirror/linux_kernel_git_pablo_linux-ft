251a67cc7e7ef39f6d888c99625deac9a51ff06c
Pablo Neira Ayuso <pablo@netfilter.org>
2023-05-10 13:58:34 +0200

    netfilter: nf_tables: add flowtable early_ingress support
    Update control plane to allow to create a flowtable in the early_ingress
    hook.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -7535,7 +7535,8 @@ static int nft_flowtable_parse_hook(const struct nft_ctx *ctx,
 			return -EINVAL;
 
 		hooknum = ntohl(nla_get_be32(tb[NFTA_FLOWTABLE_HOOK_NUM]));
-		if (hooknum != NF_NETDEV_INGRESS)
+		if (hooknum != NF_NETDEV_INGRESS &&
+		    hooknum != NF_NETDEV_EARLY_INGRESS)
 			return -EOPNOTSUPP;
 
 		priority = ntohl(nla_get_be32(tb[NFTA_FLOWTABLE_HOOK_PRIORITY]));
@@ -7572,7 +7573,14 @@ static int nft_flowtable_parse_hook(const struct nft_ctx *ctx,
 		hook->ops.hooknum	= flowtable_hook->num;
 		hook->ops.priority	= flowtable_hook->priority;
 		hook->ops.priv		= &flowtable->data;
-		hook->ops.hook		= flowtable->data.type->hook;
+		switch (hooknum) {
+		case NF_NETDEV_INGRESS:
+			hook->ops.hook	= flowtable->data.type->hook;
+			break;
+		case NF_NETDEV_EARLY_INGRESS:
+			hook->ops.hook	= flowtable->data.type->hook_list;
+			break;
+		}
 	}
 
 	return err;
